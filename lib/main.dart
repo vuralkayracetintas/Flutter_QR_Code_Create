9950f07dbaf873073541940163a6b434d4939fe0
Vural Kayra Çetintaş <73638860+vuralkayracetintas@users.noreply.github.com>
2022-10-06 00:39:30 +0300

    Flutter Create QR Code

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:qrcode/qr_code_create.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const QrCodeCreatePage(),
+    );
+  }
+}
